deac08449d709b59e4e6aeedf2dfa9844e6530df
srishanth-07 <srishanthpodduturi@gmail.com>
2025-09-27 20:50:14 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_assistance_chatbot",
+    version="0.1.0",
+    author="Podduturi Srishanth Reddy",
+    author_email="srishanthpodduturi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
